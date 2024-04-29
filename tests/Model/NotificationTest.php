a07a71b17899483a181818dff2f86534d0eabf5f
Quentin SOMAZZI <quentin.somazzi@ekino.com>
2024-04-29 23:11:08 +0200

    Fix cs

--- tests/Model/NotificationTest.php
+++ tests/Model/NotificationTest.php
@@ -1,9 +1,18 @@
 <?php
 
+/*
+ * This file is part of the Particle project.
+ *
+ * (c) Qsomazzi
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Qsomazzi\Particle\Model\Notification;
 
 test('Model notification', function () {
     $notification = new Notification('Foo');
-    
+
     expect($notification)->toBeInstanceOf(Notification::class);
 });
